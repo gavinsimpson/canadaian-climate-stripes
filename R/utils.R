7b88240bd5500c81e40db343bc75eddd078680cf
Gavin L. Simpson <ucfagls@gmail.com>
2019-07-09 10:33:12 -0600

    add n_stations() utility

---
+++ R/utils.R
@@ -0,0 +1,22 @@
+##' Returns the number of stations in the AHCCD with climate data
+##'
+##' @title Number of AHCCD stations
+##' @return The number of stations as a length-1 integer
+##' @author Gavin L. Simpson
+##'
+##' @importFrom crul HttClient
+##' @importFrom jsonlite fromJason
+n_stations <- function() {
+    ## create a HttpClient object, defining the url
+    url <- "https://geo.weather.gc.ca/geomet/features/collections/ahccd-stations/items?limit=0"
+    cli <- HttpClient$new(url = url)
+    ## do a GET request
+    res <- cli$get()
+    ## check to see if request failed or succeeded
+    ## - if succeeds this will return nothing and proceeds to next step
+    res$raise_for_status()
+    ## convert to JSON
+    js <- fromJSON(res$parse("UTF-8"))
+    ## return the number of records matched
+    js[["numberMatched"]]
+}

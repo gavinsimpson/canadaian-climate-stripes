3076eb7ecc5f3c133e8cf70ff5b728d4c7a91211
Gavin L. Simpson <ucfagls@gmail.com>
2019-07-09 11:05:44 -0600

    add ahccd_base_url() and ahccd_request() helper functions

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,9 @@
 ##' Returns the number of stations in the AHCCD with climate data
 ##'
 ##' @title Number of AHCCD stations
+##'
 ##' @return The number of stations as a length-1 integer
+##'
 ##' @author Gavin L. Simpson
 ##'
 ##' @importFrom crul HttClient
@@ -9,6 +11,25 @@
 `n_stations` <- function() {
     ## create a HttpClient object, defining the url
     url <- "https://geo.weather.gc.ca/geomet/features/collections/ahccd-stations/items?limit=0"
+    ## request data from GeoMet
+    js <- ahccd_request(url = url)
+    ## return the number of records matched
+    js[["numberMatched"]]
+}
+
+##' Make a HTTP request for AHCCD records from GeoMet
+##'
+##' @title HTTP request from AHCCD
+##' 
+##' @param url character; a URL querying GeoMet's OGC Features API
+##'
+##' @return The result of the HTTP requested, parsed from JSON.
+##'
+##' @author Gavin L. Simpson
+##'
+##' @importFrom crul HttClient
+##' @importFrom jsonlite fromJason
+`ahccd_request` <- function(url) {
     cli <- HttpClient$new(url = url)
     ## do a GET request
     res <- cli$get()
@@ -17,7 +38,28 @@
     res$raise_for_status()
     ## convert to JSON
     js <- fromJSON(res$parse("UTF-8"))
-    ## return the number of records matched
-    js[["numberMatched"]]
+    js
 }
 
+##' Base URLs for AHCCD collections on GeoMet
+##'
+##' @title URLS for AHCCD collections
+##'
+##' @param collection character; return the URL for which collection?
+##'
+##' @return A character string containing the requested URL
+##'
+##' @author Gavin L. Simpson
+##'
+##' @importFrom rlang arg_match
+##' @importFrom glue glue
+`ahccd_base_url` <- function(collection = c("stations", "annual", "seasonal",
+                                            "monthly")) {
+    collection <- arg_match(collection)
+    base_url <- "https://geo.weather.gc.ca/geomet/features/collections"
+    switch(collection,
+           stations = glue("{base_url}/ahccd-stations/"),
+           annual   = glue("{base_url}/ahccd-annual/"),
+           seasonal = glue("{base_url}/ahccd-seasonal/"),
+           monthly  = glue("{base_url}/ahccd-monthly/"))
+}
